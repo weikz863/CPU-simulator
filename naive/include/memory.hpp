42ec4f0d07bfd5502db302a4a8f99a3aeb44aa93
weikz <weikz@sjtu.edu.cn>
2025-07-22 16:22:58 +0800

    feat: add initializer

--- naive/include/memory.hpp
+++ naive/include/memory.hpp
@@ -3,6 +3,9 @@
 #define _MY_MEMORY_HPP_
 
 #include <iostream>
+#include <sstream>
+#include <string>
+#include <string_view>
 #include <unordered_map>
 #include "tools.h"
 
@@ -10,8 +13,8 @@ struct MemInput {
   Wire<32> addr;
   Wire<32> delta;
   Wire<32> value;
-  Wire<1> activate;
-  Wire<1> mode;
+  Wire<2> issue;
+  Wire<3> mode;
 };
 
 struct MemOutput {
@@ -24,28 +27,46 @@ struct MemError {
 
 struct MemModule : dark::Module<MemInput, MemOutput> {
   static unsigned const lag = 3;
-  unsigned state = 0, loc = 0, val = 0;
-  bool mod = 0; // 0 = read, 1 = write
-  std::unordered_map<unsigned, unsigned> mp = {};
+  unsigned state, loc, val;
+  Bit<2> issue_;
+  Bit<3> mode_;
+  std::unordered_map<unsigned, Bit<8>> mp;
+  MemModule() : dark::Module<MemInput, MemOutput>{}, state{0}, loc{0}, val{0}, issue_{0}, mode_{0}, mp{} {
+    unsigned place = 0, tmp = 0;
+    std::string s;
+    while (std::cin >> s) {
+      if (s[0] == '@') {
+        std::istringstream ss(std::string(s.data() + 1));
+        ss >> std::hex >> place;
+      } else {
+        std::istringstream ss(s.data());
+        ss >> std::hex >> tmp;
+        mp[place] = tmp;
+      }
+    }
+  }
 	void work() override final {
-    if (static_cast<bool>(activate)) {
+    if (static_cast<bool>(issue)) {
       if (state) {
         throw MemError();
       }
       state = 1;
       loc = static_cast<unsigned>(addr) + static_cast<unsigned>(delta);
       val = static_cast<unsigned>(value);
-      mod = static_cast<bool>(mode);
+      issue_ = issue;
+      mode_ = mode;
       fin <= false;
     } else if (state) {
       state++;
       if (state == lag) {
         fin <= true;
-        result <= (mod ? 0 : mp[loc]);
-        if (mod == 1) {
-          mp[loc] = val;
-        }
         state = 0;
+        switch (static_cast<unsigned>(Bit<5>({issue_, mode_}))) {
+          default: {
+            result <= 0;
+            break;
+          }
+        }
       }
     } else {
       fin <= false;
