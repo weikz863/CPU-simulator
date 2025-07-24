929e21e4e76105c62fffc5a4bfb913625aeb6b68
weikz <weikz@sjtu.edu.cn>
2025-07-24 17:41:37 +0800

    feat: add control logic

--- simulator/include/control.hpp
+++ simulator/include/control.hpp
@@ -25,21 +25,45 @@ struct ControlOutput {
 
 struct ControlModule : dark::Module<ControlInput, ControlOutput> {
   static constexpr int RoBCAPACITY = 1; // this cannot be changed!
-  Bit<32> current_command;
+  Bit<32> current_instruction;
   int RoBsize;
   bool mem_busy, ALU_busy;
-  Bit<32> reg[32], pc, *loading_to;
-  ControlModule() : dark::Module<ControlInput, ControlOutput>{}, current_command{0}, RoBsize{0}, 
-      mem_busy{0}, ALU_busy{0}, reg{}, pc{0}, loading_to{&reg[0]} {
+  Bit<32> reg[32], pc, logic, *loading_to, *ALU_to;
+  Bit<32> default_jmp; // this should be in reserve station, but there isn't.
+  ControlModule() : dark::Module<ControlInput, ControlOutput>{}, current_instruction{0}, RoBsize{0}, 
+      mem_busy{0}, ALU_busy{0}, reg{}, pc{0}, loading_to{&reg[0]}, ALU_to{&reg[0]} {
     ;
   }
   void work() {
     if (static_cast<unsigned>(mem_done)) {
       mem_busy = false;
       *loading_to = mem_result;
+      if (loading_to == &current_instruction) {
+        parse_instruction();
+      } else { // this is a load instruction finished
+        commit();
+      }
     }
-    if (!mem_busy && RoBsize < RoBCAPACITY) {
-      loading_to = &current_command;
+    if (!mem_busy && RoBsize < RoBCAPACITY) { // load new instruction
+      loading_to = &current_instruction;
+      mem_busy = true;
+      RoBsize++;
+      mem_addr <= pc;
+      mem_delta <= 0;
+      mem_issue <= 1; // 1 = load
+      mem_mode <= 2; // 2 = word
+      pc += 4;
+    }
+    if (static_cast<unsigned>(ALU_done)) {
+      ALU_busy = false;
+      *ALU_to = ALU_result;
+      if (ALU_to == &logic) { // encounter branch
+        if (static_cast<unsigned>(logic)) { // all guessing "not jump"
+          pc = default_jmp;
+        }
+      } else { // this is an arithmetic instruction finished
+        commit();
+      }
     }
     if (static_cast<unsigned>(mem_issue)) {
       mem_issue <= 0;
